cf106bb84f0d74ea067968e69ec45d564001f605
Atom <stevecoulter99@gmail.com>
2020-10-28 11:17:06 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"log"
+)
+
+const token string = ""
+
+var (
+	BotId string
+	Prefix string = "!"
+)
+
+func main() {
+	log.Printf("Starting Bot...")
+
+	dg, err := discordgo.New("Bot " + token)
+
+	if err != nil {
+		log.Printf("%s", err.Error())
+		return
+	}
+
+	u, err := dg.User("@me")
+
+	if err != nil {
+		log.Printf("%s", err.Error())
+		return
+	}
+
+	BotId = u.ID
+	err = dg.Open()
+
+	if err != nil {
+		log.Printf("%s", err.Error())
+		return
+	}
+
+	log.Printf("Bot is ready!")
+
+	<-make(chan struct{})
+	return
+}
